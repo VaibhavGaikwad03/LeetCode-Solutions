2819ccce70157ffc73b967cb736b4d654d188abc
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-05-08 21:08:04 +0530

    Add files via upload

---
+++ 0021_merge_two_sorted_lists.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+
+public:
+    ListNode *mergeTwoLists(ListNode *list1, ListNode *list2)
+    {
+        if (list1 == NULL)
+        {
+            list1 = list2;
+            list2 = NULL;
+            return list1;
+        }
+
+        if (list2 == NULL)
+            return list1;
+
+        ListNode *ptr = new ListNode();
+        ListNode *ret = ptr;
+
+        while (list1 && list2)
+        {
+            if (list1->val <= list2->val)
+            {
+                ptr->next = new ListNode(list1->val);
+                list1 = list1->next;
+            }
+            else
+            {
+                ptr->next = new ListNode(list2->val);
+                list2 = list2->next;
+            }
+
+            ptr = ptr->next;
+        }
+
+        while (list1)
+        {
+            ptr->next = new ListNode(list1->val);
+            list1 = list1->next;
+            ptr = ptr->next;
+        }
+
+        while (list2)
+        {
+            ptr->next = new ListNode(list2->val);
+            list2 = list2->next;
+            ptr = ptr->next;
+        }
+
+        return ret->next;
+    }
+};
