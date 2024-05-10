133ab5220588e8c890e0640c68cc55d16c633fce
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2024-05-10 21:03:39 +0530

    Add files via upload

---
+++ 0026_remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        vector<int> unique;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i == 0)
+                unique.push_back(nums[i]);
+
+            if (unique.back() != nums[i])
+                unique.push_back(nums[i]);
+        }
+
+        nums = unique;
+        return nums.size();
+    }
+};
